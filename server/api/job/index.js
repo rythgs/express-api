f5099f2ecbd09309965468aceae7f096a31f89b4
Ryota Higashi <me@higashiryota.com>
2017-11-24 22:07:54 +0900

    file upload sample

--- server/api/job/index.js
+++ server/api/job/index.js
@@ -5,6 +5,24 @@ var controller = require('./job.controller');
 
 var router = express.Router();
 
+import path from 'path';
+import multer from 'multer';
+
+const upload = multer({
+  dest: './uploads',
+  fileFilter(req, file, callback) {
+    const filetypes = /zip/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    if (mimetype && extname) {
+      return callback(null, true);
+    }
+    return callback(null, false);
+  }
+});
+
+router.post('/upload', upload.single('thumbnail'), controller.upload);
+
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
